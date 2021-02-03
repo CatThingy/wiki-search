f7d6866e616799aa5f5c9456dbef8c65d2c4e3d9
CatThingy <andyc.andychen@gmail.com>
2021-02-02 18:50:14 -0800

    Added searching

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
+const fetch = require("node-fetch");
 const config = require("./config.json");
+
 
 const client = new Discord.Client();
 const settings = new Enmap({
@@ -39,49 +41,109 @@ client.on("message", message => {
         case "config":
         case "settings":
         case "set":
-            // Only allow administrators to edit settings, no settings can change in DMs
-            if (!message.guild || !message.member.hasPermission("MANAGE_GUILD")) { return; }
-
-            if (args[0] === undefined) {
-                message.channel.send({
-                    embed: {
-                        title: "Settings",
-                        fields: [
-                            {
-                                name: "Prefix",
-                                value: serverSettings.prefix
-                            },
-                            {
-                                name: "Wiki: ",
-                                value: serverSettings.wiki
-                            }
-                        ]
-                    }
-                });
-            }
-            else if (args[1] === undefined) {
-                if (args[0] == "prefix") {
-                    message.channel.send("```\nPrefix: " + serverSettings.prefix + "\n```");
-                }
-                else if (args[0] == "wiki") {
-                    message.channel.send("```\nPrefix: " + serverSettings.wiki + "\n```");
-                }
-            }
-            else {
-                if (args[0] == "prefix") {
-                    message.channel.send("```\nPrefix has been changed to " + args[1] + "\n```");
-                    settings.set(message.guild.id, args[1], "prefix");
-                }
-                else if (args[0] == "wiki") {
-                    message.channel.send("```\nWiki has been changed to " + args[1] + "\n```");
-                    settings.set(message.guild.id, args[1], "prefix");
-                }
-            }
+            cmdConfig(args, message, serverSettings);
             break;
 
-
+        case "search":
+        case "s":
+        case "find":
+        case "wiki":
+        case "article":
+            cmdSearch(args, message, serverSettings);
+            break;
         default:
             return;
     }
-
 });
+
+function cmdConfig(args, message, serverSettings) {
+    // Only allow administrators to edit settings, no settings can change in DMs
+    if (!message.guild || !message.member.hasPermission("MANAGE_GUILD")) { return; }
+
+    if (args[0] === undefined) {
+        message.channel.send({
+            embed: {
+                title: "Settings",
+                fields: [
+                    {
+                        name: "Prefix",
+                        value: serverSettings.prefix
+                    },
+                    {
+                        name: "Wiki: ",
+                        value: serverSettings.wiki
+                    }
+                ]
+            }
+        });
+    }
+    else if (args[1] === undefined) {
+        if (args[0] == "prefix") {
+            message.channel.send("```\nPrefix: " + serverSettings.prefix + "\n```");
+        }
+        else if (args[0] == "wiki") {
+            message.channel.send("```\Wiki: " + serverSettings.wiki + "\n```");
+        }
+    }
+    else {
+        if (args[0] == "prefix") {
+            message.channel.send("```\nPrefix has been changed to " + args[1] + "\n```");
+            settings.set(message.guild.id, args[1], "prefix");
+        }
+        else if (args[0] == "wiki") {
+            message.channel.send("```\nWiki has been changed to " + args[1] + "\n```");
+            settings.set(message.guild.id, args[1], "wiki");
+        }
+    }
+}
+
+async function cmdSearch(args, message, serverSettings) {
+    let sentMessage = await message.channel.send("Searching...")
+
+    const searchTerm = encodeURIComponent(args.join(""));
+    let searchURL;
+    if ((serverSettings.wiki).includes("fandom") || (serverSettings.wiki).includes("gamepedia")) {
+        searchURL = "https://" + serverSettings.wiki + "/api.php"
+    }
+    else {
+        searchURL = "https://" + serverSettings.wiki + "/w/api.php"
+    }
+
+    let pageTitle;
+    let pageId;
+    await fetch(searchURL + "?action=query&format=json&list=search&srlimit=1&srsearch=" + searchTerm)
+        .then(data => data.json())
+        .then(data => {
+            pageTitle = decodeURIComponent(data.query.search[0].title);
+            pageId = data.query.search[0].pageid
+        });
+
+    let categories = [];
+
+    await fetch(searchURL + "?action=query&format=json&prop=categories&titles=" + pageTitle)
+        .then(data => data.json())
+        .then(data => {
+            if (data.query.pages[pageId].categories !== undefined) {
+                for (let cat of data.query.pages[pageId].categories) {
+                    categories.push(cat.title.replace("Category:", ""));
+                }
+            }
+        });
+
+
+    let pageUrl = "https://";
+    if (serverSettings.wiki.includes("fandom") || serverSettings.wiki.includes("gamepedia")) {
+        pageUrl += serverSettings.wiki + "/" + encodeURIComponent(pageTitle);
+    }
+    else {
+        pageUrl += serverSettings.wiki + "/wiki/" + encodeURIComponent(pageTitle);
+    }
+
+    sentMessage.edit("", {
+        embed: {
+            url: pageUrl,
+            title: pageTitle,
+            description: "Categories: " + (categories.length ? categories.join() : "None") 
+        }
+    });
+}
